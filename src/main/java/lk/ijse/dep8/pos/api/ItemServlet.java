7022f1f69d7372ae6e26b0f5b3418d08e8b47f2c
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-01 16:13:14 +0530

    finalize the save items feature

---
+++ src/main/java/lk/ijse/dep8/pos/api/ItemServlet.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep8.pos.api;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ItemServlet", value = "/ItemServlet")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/lk/ijse/dep8/pos/dto/ItemDTO.java
@@ -0,0 +1,2 @@
+package lk.ijse.dep8.pos.dto;public class ItemDTO {
+}
